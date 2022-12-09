b0b0a4e8b98d9cf65ef5d71189d5962eaebef123
Amir <amir.kashapov@protonmail.ch>
2022-12-10 00:15:54 +1100

    feat: scoreboard and substring match on curse words

--- bot.py
+++ bot.py
@@ -2,13 +2,30 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 updater = Updater(token="BOT_TOKEN", use_context=True)
 groups = updater.bot.get_my_groups()
-for group in groups:
-    messages = group.history()
 
-    for message in messages:
-    if "swear word substring" in message.text: #to add conditions
-        #identify who blashpemed and punish appropriately here
-        group.send_message("The substring was found in this message: " + message.text)
-        user = message.from_user
-        user_name = user.first_name
+scoreboard = {} 
+
+for group in groups:
+
+    if group.username == "GROUP_ID"
+        
+        for message in group.history():
+
+            #substring condition
+            curse_list = ["curse1", "curse2"]
+            curse_score = sum([1 if curse.lower() in message.text.lower() else 0 for curse in curse_list)]
+
+            if curse_score>0:
+                #identify who blashpemed and punish appropriately
+                user = message.from_user
+                user_name = user.first_name
+
+                if user_name in scoreboard:
+                    scoreboard[user_name]+=curse_score
+                else:
+                    scoreboard[user_name]=curse_score
+
+                sorted_scoreboard = sorted(scoreboard, key=scoreboard.get, reverse=True)
+                results = "\n".join([user_name+": "+scoreboard[user_name] for user_name in scoreboard])
+                group.send_message("Someone has blasphemed...\n\nNew scoreboard:\n"+results)
 
